880970cab260a7ea09811934603fdbb56ad075a2
Tom-Bumhead <thomas.james.marsden@gmail.com>
2022-03-06 19:26:25 +0000

    point light fixed

--- hw3d/PhongPS.hlsl
+++ hw3d/PhongPS.hlsl
@@ -4,12 +4,12 @@ cbuffer LightCBuf
 };
 
 static const float3 materialColour = { 0.7f, 0.7f, 0.9f };
-static const float3 ambient = { 0.15f, 0.15f, 0.15f };
+static const float3 ambient = { 0.05f, 0.05f, 0.05f };
 static const float3 diffuseColour = { 1.0f, 1.0f, 1.0f };
 static const float diffuseIntensity = 1.0f;
 static const float attConst = 1.0f;
-static const float attLin = 1.0f;
-static const float attQuad = 1.0f;
+static const float attLin = 0.045f;
+static const float attQuad = 0.0075f;
 
 
 float4 main(float3 worldPos : Position, float3 n : Normal) : SV_TARGET
